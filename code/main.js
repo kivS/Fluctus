79262c5bb0628d2ce8949defd73eb8e2a7a066b7
Vik Borges <v1k@protonmail.com>
2017-01-28 00:44:25 +0000

    added base files; clear for take off

---
+++ code/main.js
@@ -0,0 +1,18 @@
+const {electron, app, BrowserWindow} = require('electron');
+
+
+//*****************************************************
+//			   APP Events						   
+//									  				   				
+//*****************************************************
+app.on('ready', start);
+
+
+
+//*****************************************************
+//			   Program Starter						   
+//									  				   				
+//*****************************************************
+function start(){
+	console.log(`HEYYYYYY OHHH BOIIIA LET'S BEGIN!!!!!`);
+}
