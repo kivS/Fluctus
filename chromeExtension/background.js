fa3c30bfc1d1071e2beaf8bd4573ee1dfa96b57d
Vik Borges <v1k@protonmail.com>
2017-02-10 17:27:22 +0000

    extension can now detect a youtube page and show the pageAction acordingly

--- chromeExtension/background.js
+++ chromeExtension/background.js
@@ -2,18 +2,31 @@ console.log('Hello there from the background......huhhh');
 
 
 // When extension is installed on upgraded
-chrome.runtime.onInstalled.addListener( details => {
+chrome.runtime.onInstalled.addListener( () => {
 	// Replace all rules
 	chrome.declarativeContent.onPageChanged.removeRules(undefined, () => {
 		// With a new rule
 		chrome.declarativeContent.onPageChanged.addRules([
 		 	{
 		 		// Trigger me!!
-		 		conditions: [],
+		 		conditions: [
+		 			new chrome.declarativeContent.PageStateMatcher({
+		 				pageUrl: { hostContains: 'youtube.com' }
+		 			})
+		 		],
 
 		 		// Shows the page_action
-		 		actions: []
+		 		actions: [
+		 			new chrome.declarativeContent.ShowPageAction()
+
+		 		]
 		 	}
 		]);
 	});
+});
+
+
+// Page_action click event
+chrome.pageAction.onClicked.addListener( tab => {
+	console.log('page_action clicked..', tab);
 });
