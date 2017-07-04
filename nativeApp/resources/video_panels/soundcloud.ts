78e13c6477e9fb0db54243a0ed391391df4b6188
Vik Borges <v1k@protonmail.com>
2017-07-04 22:49:30 +0100

    removed logs from soundcloud

--- nativeApp/resources/video_panels/soundcloud.ts
+++ nativeApp/resources/video_panels/soundcloud.ts
@@ -1,12 +1,12 @@
 // removes Object.defineProperty(exports, "__esModule", { value: true }); from compiled js
 export = 0;
 
-import {remote} from 'electron';
+//import {remote} from 'electron';
 import * as url from 'url';
-const log = remote.getGlobal('logger');
-const TAG = 'From Soundcloud Panel - ';
+//const log = remote.getGlobal('logger');
+//const TAG = 'From Soundcloud Panel - ';
 
-log.info(TAG, "Hello from video panel!!!");
+//log.info(TAG, "Hello from video panel!!!");
 
 // get payload and parse it into object 
 const payload = url.parse(location.search, true).query;
@@ -16,6 +16,7 @@ console.log('Payload: ', payload);
 // Load soundcloud
 declare var SC: any;
 
-var soundcloud = SC.Widget(document.getElementById('player'));
+const soundcloud = SC.Widget(document.getElementById('player'));
 
 soundcloud.load(payload.video_url);
+
